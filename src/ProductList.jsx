a4d6f3748124205f0facb8739c8299a2afd49197
ekenneth11 <kennethbautista1512@gmail.com>
2025-02-26 21:26:29 -0500

    added the products into the UI

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -246,7 +246,11 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+  const handleAddToCart = (e) => {
+    e.preventDefault();
+  };
     return (
+        <>
         <div>
              <div className="navbar" style={styleObj}>
             <div className="tag">
@@ -264,17 +268,33 @@ const handlePlantsClick = (e) => {
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
                 <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
-            </div>
+            </div> 
         </div>
         {!showCart? (
         <div className="product-grid">
-
+            {plantsArray.map((category, index)=>(
+                <div key={index}>
+                    <h1><div>{category.category}</div></h1>
+                    <div className='product-list'>
+                        {category.plants.map((plants, plantIndex) => (
+                            <div className='product-card' key={plantIndex}>
+                                <img className='product-image' src={plants.image} alt={plants.name} />
+                                <div className='product-title'>{plants.name}</div>
+                                <div className='product-title'>{plants.description}</div>
+                                <div className='product-title'>{plants.cost}</div>
+                                <button className='product-button' onClick={() => handleAddToCart(plants)}>Add To Cart</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
 
         </div>
  ) :  (
     <CartItem onContinueShopping={handleContinueShopping}/>
 )}
     </div>
+    </>
     );
 }
 
